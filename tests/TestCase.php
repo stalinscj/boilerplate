681d7d0b1c29e6f7a6bb09c64febb267316f4f38
stalinscj <ssanchez684@gmail.com>
2022-03-13 13:14:40 -0400

    Add signInWithPermissionsTo to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Models\User;
+use App\Models\Permission;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -20,6 +21,23 @@ abstract class TestCase extends BaseTestCase
         $user = $user ?? User::factory()->createOne();
 
         $this->actingAs($user);
+
+        return $user;
+    }
+
+    /**
+     * Returns a User with Permissions for routes names after sign in
+     *
+     * @param  array|string  $permissions
+     * @param  \App\Models\User|null  $user
+     * @return \App\Models\User
+     */
+    protected function signInWithPermissionsTo($routesNames, $user = null)
+    {
+        $user = $this->signIn($user);
+        $permissions = Permission::whereIn('route_name', collect($routesNames))->get();
+
+        $user->givePermissionTo($permissions);
 
         return $user;
     }
